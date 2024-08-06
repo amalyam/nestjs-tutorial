0ec3d502a4a5fea6b146bc412e1188044d6fe706
Amalya Megerman <amalyamegerman@gmail.com>
2024-08-06 17:08:15 -0400

    set up dependency injection (DI) system

--- second-app/messages/src/messages/messages.service.ts
+++ second-app/messages/src/messages/messages.service.ts
@@ -1,5 +1,12 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+// @Injectable marks this class for registration inside
+// dependency injection (DI) container (i.e. this class
+// can be injected as a dependency into other classes)
+// Singleton scope - only one instance of the class is
+// created and shared across the entire application
+@Injectable()
 export class MessagesService {
   constructor(public messagesRepo: MessagesRepository) {}
 
